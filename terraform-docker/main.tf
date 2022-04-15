85469ecb35d0ad50462575c4a39589711061ee9c
vvafwgsv <handlarz.parapetami@gmail.com>
2022-04-15 17:52:04 +0200

    fixed vim tabs

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -10,27 +10,27 @@ terraform {
 provider "docker" {}
 
 variable "image_name" {
-	type 				= string
+	type = string
 	description = "The literal of either 'nginx-docker:final' or 'flask-docker:final'." 
 
 	validation {
-		condition			= var.image_name == "flask-docker:final" || var.image_name == "nginx-docker:final"
+		condition = var.image_name == "flask-docker:final" || var.image_name == "nginx-docker:final"
 		error_message = "The image_name must be either 'flask-docker:final' or 'nginx-docker:final'."
 	}
 }
 
 variable "network_config" {
-	type			  = object({
-									external_port = number
-									url	 					= string
-								})
+	type = object({
+			external_port = number
+			url	= string
+		})
 	description = "The network_config variable is used to set desired proxy the docker container is to adopt. network_config.port as <number> specifies EXTERNAL_PORT for docker container, network_config.url as <string> denotes the host."	
 	validation {
-		condition 		= (var.network_config.external_port == 8081)
+		condition = (var.network_config.external_port == 8081)
 		error_message = "The external port is expected to be 8081."
 	}
 	validation {
-		condition			= (var.network_config.url == "localhost")
+		condition = (var.network_config.url == "localhost")
 		error_message = "The URL is expected to be 'localhost'."
 	}
 }
@@ -46,7 +46,7 @@ resource "docker_container" "created" {
 	ports {
 		external = var.network_config.external_port
 		internal = (var.image_name == "flask-docker:final" ? 5000 : 80)
-		ip			 = var.network_config.url
+		ip = var.network_config.url
 	}
 }
 
